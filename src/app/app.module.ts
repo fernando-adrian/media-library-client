b5f53f07fcbafaa2f9fb85fe364e55ba653eab5c
fernando robles <advancexz@gmail.com>
2023-03-29 10:14:26 -0700

    app module added with media item and star rating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MediaItemComponent } from './components/media-item/media-item.component';
+import { StarRatingsComponent } from './components/star-ratings/star-ratings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MediaItemComponent,
+    StarRatingsComponent,
   ],
   imports: [
     BrowserModule,
