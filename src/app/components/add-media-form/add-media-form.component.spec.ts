8077956ba3bf7ff4783dbe7c4109fc9d94fbb10e
fernando robles <advancexz@gmail.com>
2023-03-30 18:31:38 -0700

    add media form added

---
+++ src/app/components/add-media-form/add-media-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddMediaFormComponent } from './add-media-form.component';
+
+describe('AddMediaFormComponent', () => {
+  let component: AddMediaFormComponent;
+  let fixture: ComponentFixture<AddMediaFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddMediaFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddMediaFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/add-media-form/add-media-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-media-form',
+  templateUrl: './add-media-form.component.html',
+  styleUrls: ['./add-media-form.component.scss']
+})
+export class AddMediaFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
