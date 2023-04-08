309620de30c86f5a21203f836f3bcb1a836fe5b0
fernando robles <advancexz@gmail.com>
2023-04-07 17:59:19 -0700

    - add media source form compoenent layout updated - modal added to add media source - table added to show all media sources from backend - methods added to support get all sources, delete sources and add sources

--- src/app/components/add-media-source-form/add-media-source-form.component.ts
+++ src/app/components/add-media-source-form/add-media-source-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MediaSourceService } from 'src/app/services/media-source-service';
+import { MediaSource } from 'src/app/models/mediaSource.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add-media-source-form',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMediaSourceFormComponent implements OnInit {
 
-  constructor() { }
+
+  public mediaSourceForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    source: new FormControl('',[Validators.required]),
+    description: new FormControl('')
+  });
+  public mediaSource: MediaSource;
+  public mediaSourceList?: MediaSource[];
+  public idToDelete?: number;
+
+  constructor(private mediaSourceService: MediaSourceService) {
+    this.mediaSource = {
+      id: 0,
+      name: "",
+      source: "",
+      description: '',
+      image: ''
+    };
+  }
 
   ngOnInit(): void {
+    this.getAllSources().subscribe((data)=>{
+      console.log(data);
+      this.mediaSourceList = data.map((media: any) => {
+        return {
+          id: media.id,
+          name: media.name,
+          source: media.source,
+          description: media.description,
+          image: media.image
+        }
+      });
+    });
+  }
+
+  addMediaSource(mediaSource: MediaSource){
+    this.mediaSourceService.addMediaSource(mediaSource);
+  }
+
+  deleteMediaSource(){
+    console.log("delete media source with id", this.idToDelete);
+    this.mediaSourceService.deleteMediaSource(this.idToDelete);
+  }
+
+  getAllSources(): Observable<any>{
+    console.log("this.getAllSources");
+    return this.mediaSourceService.getAllSources();
+  }
+
+  onSubmit(form: FormGroup): void{
+    console.log("asdasd");
+    console.log(form);
+    this.mediaSource = {
+      id: 0,
+      name: form.value.name,
+      source: form.value.source,
+      description: form.value.description,
+      image: null
+    }
+    this.addMediaSource(this.mediaSource);
   }
 
 }
